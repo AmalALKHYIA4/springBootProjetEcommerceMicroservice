f1edc317dc8aeeee82633d3746aa379a65ee7d6e
AmalALKHYIA4 <alkhyiaamal4@gmail.com>
2025-10-31 18:26:34 +0100

    add-spring cloud config server to the initial project

---
+++ configserver/src/test/java/configm_mgmt/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package configm_mgmt.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
